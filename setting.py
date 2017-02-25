78d86826e1db3f6f75204b7278a9f60902bb1e17
wwj <wuwenjie718@gmail.com>
2017-02-25 10:22:51 +0800

    重构日志系统;将设置信息移到setting文件

---
+++ setting.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python
+# encoding: utf-8
+from __future__ import unicode_literals
+import os
+import logging
+
+logger = logging.getLogger(__name__)
+
+USE_LEANCLOUD = False #默认不使用消息云存储 但是头像一直在用它
+DEBUG = False #使用环境变量，不必修改文件
+
+is_debug = os.getenv("DEBUG", None) #env DEBUG=True python wechat_group_bot.py(fish)  DEBUG=True python wechat_group_bot.py(bash/zsh)
+if is_debug:
+        DEBUG = is_debug
+
+
+FORCED_IN_ACTION = False # 是够是活动期间
+
+# feature action
+# 管理员名字
+if DEBUG:
+    ACTION_ADMIN=["种瓜",]
+else:
+    ACTION_ADMIN=["张俊",]
+ACTION_KEYWORD = "提问开始"
+ACTION_TIME=60*60*2 #2 hour
+
